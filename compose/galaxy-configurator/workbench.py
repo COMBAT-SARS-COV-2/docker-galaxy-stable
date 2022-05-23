26521a5be45c3f512a5346ab95417b4bbf39b9fc
Zipho Mashologu <zipho.m@gmail.com>
2022-05-23 12:25:39 +0200

    blacked

--- compose/galaxy-configurator/workbench.py
+++ compose/galaxy-configurator/workbench.py
@@ -11,9 +11,11 @@ from tqdm.auto import tqdm
 
 CONTEXT_SETTINGS = dict(
     default_map={
-        'download_jar': {'illumina_version': config('ILLUMINA_VERSION', default='latest'),
-                         'nanopore_version': config('NANOPORE_VERSION', default='latest'),
-                         'access_token': config('GITHUB_ACCESS_TOKEN', default=False)}
+        "download_jar": {
+            "illumina_version": config("ILLUMINA_VERSION", default="latest"),
+            "nanopore_version": config("NANOPORE_VERSION", default="latest"),
+            "access_token": config("GITHUB_ACCESS_TOKEN", default=False),
+        }
     }
 )
 
@@ -37,9 +39,13 @@ def workbench():
 
 
 @workbench.command()
-@click.option('--illumina-version', default='latest', help='Illumina (SARS-COV-2) release version')
-@click.option('--nanopore-version', default='latest', help='Nanopore (SARS-COV-2) release version')
-@click.option('--access-token', help='Your unexpired & valid github access token')
+@click.option(
+    "--illumina-version", default="latest", help="Illumina (SARS-COV-2) release version"
+)
+@click.option(
+    "--nanopore-version", default="latest", help="Nanopore (SARS-COV-2) release version"
+)
+@click.option("--access-token", help="Your unexpired & valid github access token")
 def download_jar(illumina_version, nanopore_version, access_token):
     """
     This command downloads the workbench plugins from github
@@ -48,42 +54,55 @@ def download_jar(illumina_version, nanopore_version, access_token):
     try:
         g = Github(access_token)
     except GithubException as e:
-        logger.error('Github token is missing or invalid')
+        logger.error("Github token is missing or invalid")
         raise click.ClickException(f"Something went wrong: {repr(e)}")
 
-    plugin_versions = {'irida-plugin-sars-cov-2-illumina': illumina_version,
-                       'irida-plugin-sars-cov-2-nanopore': nanopore_version}
+    plugin_versions = {
+        "irida-plugin-sars-cov-2-illumina": illumina_version,
+        "irida-plugin-sars-cov-2-nanopore": nanopore_version,
+    }
     # get the plugin assets (jars)
-    for repo in g.get_organization('combat-sars-cov-2').get_repos():
+    for repo in g.get_organization("combat-sars-cov-2").get_repos():
         version = plugin_versions.get(repo.name, None)
         if version:
             release = None
-            if version == 'latest':
+            if version == "latest":
                 try:
                     release = repo.get_latest_release()
                 except GithubException as e:
-                    logger.error("Error, seems we can't find the latest release package")
+                    logger.error(
+                        "Error, seems we can't find the latest release package"
+                    )
                     raise click.ClickException(f"Something went wrong: {repr(e)}")
             else:
                 try:
                     release = repo.get_release(version)
                 except GithubException as e:
-                    logger.error(f"Error, seems we can't find {version} release package")
+                    logger.error(
+                        f"Error, seems we can't find {version} release package"
+                    )
                     raise click.ClickException(f"Something went wrong: {repr(e)}")
 
-            if release is None: raise click.ClickException("Something went wrong: release is not found")
+            if release is None:
+                raise click.ClickException("Something went wrong: release is not found")
 
             logger.info(f"Download begin for plugin package: {release.url.encode()}")
             for asset in release.get_assets():
                 logger.info(f"IRIDA Plugin Jar: {asset.browser_download_url}")
 
-                response = getattr(urllib, 'request', urllib).urlopen(asset.browser_download_url)
+                response = getattr(urllib, "request", urllib).urlopen(
+                    asset.browser_download_url
+                )
                 file_name = os.path.join(CURRENT_DIR, f"sources/plugins/{asset.name}")
 
                 ensure_dir(file_name)
-                with tqdm.wrapattr(open(file_name, "wb"), "write",
-                                   miniters=1, desc=asset.browser_download_url.split('/')[-1],
-                                   total=getattr(response, 'length', None)) as fout:
+                with tqdm.wrapattr(
+                    open(file_name, "wb"),
+                    "write",
+                    miniters=1,
+                    desc=asset.browser_download_url.split("/")[-1],
+                    total=getattr(response, "length", None),
+                ) as fout:
                     for chunk in response:
                         fout.write(chunk)
             logger.info(f"Downloading is done: {release.url.encode()}")
@@ -105,7 +124,11 @@ def extract_jar():
     relevant folder structures to be processed later
     """
     # file_names from arguments
-    file_names = [f for f in os.listdir(PATH_TO_PLUGINS) if os.path.isfile(os.path.join(PATH_TO_PLUGINS, f))]
+    file_names = [
+        f
+        for f in os.listdir(PATH_TO_PLUGINS)
+        if os.path.isfile(os.path.join(PATH_TO_PLUGINS, f))
+    ]
     for file_name in file_names:
         unzip(file_name)
 
@@ -145,7 +168,8 @@ def is_archive(file_name):
     Check if the extension of (file) is valid
     """
     ext = file_name[-4:]
-    if ext in [".jar"]: return True
+    if ext in [".jar"]:
+        return True
     return False
 
 
@@ -166,16 +190,28 @@ def deploy_plugin():
 
 
 @workbench.command()
-@click.option('--galaxy', default='http://nginx:90', help='The targeted Galaxy instance')
-@click.option('--user', default='admin@galaxy.org', help='The username to use accessing the galaxy instance')
-@click.option('--password', default='password', help='Password for the user')
-@click.option('--api-key', default='fakekey', help='API Key token generated for the user')
+@click.option(
+    "--galaxy", default="http://nginx:90", help="The targeted Galaxy instance"
+)
+@click.option(
+    "--user",
+    default="admin@galaxy.org",
+    help="The username to use accessing the galaxy instance",
+)
+@click.option("--password", default="password", help="Password for the user")
+@click.option(
+    "--api-key", default="fakekey", help="API Key token generated for the user"
+)
 def install_tools(galaxy, user, password, api_key):
     """
     This command installs the tools in galaxy
     """
     logger.info("Make the singularity magic happen")
-    file_names = [f for f in os.listdir(PATH_TO_PLUGINS) if os.path.isfile(os.path.join(PATH_TO_PLUGINS, f))]
+    file_names = [
+        f
+        for f in os.listdir(PATH_TO_PLUGINS)
+        if os.path.isfile(os.path.join(PATH_TO_PLUGINS, f))
+    ]
 
     tool_files = [get_fullpath_for_tool_yaml(f) for f in file_names]
     plugins_tools = [read_tool_set_file(file) for file in tool_files]
@@ -184,8 +220,10 @@ def install_tools(galaxy, user, password, api_key):
     for tools in plugins_tools:
         for t in tools:
             try:
-                command = f'shed-tools install -g {galaxy} -a "{api_key}" -u "{user}" -p "{password}" --toolshed {t["tool_shed_url"]} --skip_install_resolver_dependencies ' \
-                          f'--skip_install_repository_dependencies --name "{t["name"]}" --owner "{t["owner"]}" --revisions {" ".join(t["revisions"])} --section_label "{t["tool_panel_section_label"]}"'
+                command = (
+                    f'shed-tools install -g {galaxy} -a "{api_key}" -u "{user}" -p "{password}" --toolshed {t["tool_shed_url"]} --skip_install_resolver_dependencies '
+                    f'--skip_install_repository_dependencies --name "{t["name"]}" --owner "{t["owner"]}" --revisions {" ".join(t["revisions"])} --section_label "{t["tool_panel_section_label"]}"'
+                )
                 process = subprocess.Popen(command, shell=True)
                 status = os.waitpid(process.pid, 0)[1]
             except Exception as e:
@@ -195,10 +233,12 @@ def install_tools(galaxy, user, password, api_key):
 
 def get_fullpath_for_tool_yaml(file):
     """path to the tool yaml file"""
-    base_dir = os.path.join(PATH_TO_PLUGINS, os.path.join(file + '.contents', 'workflows/'))
+    base_dir = os.path.join(
+        PATH_TO_PLUGINS, os.path.join(file + ".contents", "workflows/")
+    )
     dirs = os.walk(base_dir)
     dd = [dir_names for dir_names in sorted(dirs)]
-    return os.path.join(dd[-1][0], 'tools.yaml')
+    return os.path.join(dd[-1][0], "tools.yaml")
 
 
 def read_tool_set_file(tool_file):
@@ -208,7 +248,7 @@ def read_tool_set_file(tool_file):
     with open(tool_file) as file:
         # The FullLoader parameter handles the conversion from YAML
         # scalar values to Python the dictionary format
-        return [t for t in yaml.load(file, Loader=yaml.FullLoader)['tools']]
+        return [t for t in yaml.load(file, Loader=yaml.FullLoader)["tools"]]
 
 
 @workbench.command()
@@ -219,5 +259,5 @@ def install_wf():
     pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     workbench()
